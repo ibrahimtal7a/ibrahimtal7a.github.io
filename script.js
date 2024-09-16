52309f35691dfc981e9de0fe6bfe8bd3a8f0592d
Ibrahim Talha <157985662+ibrahimtal7a@users.noreply.github.com>
2024-09-16 19:35:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+function loadScript(src, callback) {
+  const script = document.createElement("script");
+  script.src = src;
+  script.onload = callback;
+  document.head.appendChild(script);
+}
+loadScript("https://cdn.tailwindcss.com", function () {
+  console.log("Tailwind CSS loaded");
+});
+loadScript(
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js",
+  function () {
+    console.log("Font Awesome loaded");
+  }
+);
+
+function checkVisible() {
+  const sections = document.querySelectorAll(".section");
+  sections.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+    const visible = rect.top >= 0 && rect.bottom <= window.innerHeight;
+    if (visible) {
+      section.classList.add("visible");
+    }
+  });
+}
+document.addEventListener("DOMContentLoaded", (event) => {
+  checkVisible();
+  window.addEventListener("scroll", checkVisible);
+
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+});
+
+let currentIndex = 0;
+const visibleBadges = 2;
+
+function moveSlider(step) {
+  const slider = document.querySelector(".slider");
+  const badges = document.querySelectorAll(".badge");
+  const totalBadges = badges.length;
+
+  currentIndex += step;
+  if (currentIndex < 0) {
+    currentIndex = 0;
+  } else if (currentIndex > totalBadges - visibleBadges) {
+    currentIndex = totalBadges - visibleBadges;
+  }
+
+  slider.style.transform = `translateX(${-currentIndex * 160}px)`;
+}
